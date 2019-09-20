37b92dfc9fcd17d0cbd1956ce3e8d59067962d60
Rodrigo <rodgomesc@gmail.com>
2019-09-20 15:31:42 -0300

    feat(signup) added signup page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,11 +1,44 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/images/tecmar.png';
+import history from '../../services/history';
+import {
+  Container,
+  SignUpHeader,
+  Wrapper,
+  BgImage,
+  SignUpContent
+} from './styles';
 
 export default function SignUp() {
+  function handleLogin() {
+    history.push('/dashboard');
+  }
   return (
-    <div>
-      <h1>hello from SignUp</h1>
-    </div>
+    <BgImage>
+      <Container>
+        <Wrapper>
+          <SignUpHeader>
+            <img src={logo} height="48" alt="logo" />
+            <h1>House Control Program</h1>
+          </SignUpHeader>
+          <SignUpContent>
+            <h2>login</h2>
+            <p>Entre com email e senha para acessar o painel administrativo</p>
+            <input placeholder="Entre com seu nome completo" type="text" />
+            <input placeholder="Digite um usuário" type="text" />
+            <input placeholder="Digite uma senha" type="password" />
+            <input placeholder="Confirme a senha" type="password" />
+            <button onClick={handleLogin}>Criar Conta</button>
+          </SignUpContent>
+        </Wrapper>
+        <p>
+          Já tem uma conta?
+          <strong>
+            <Link to="/">Fazer login</Link>
+          </strong>
+        </p>
+      </Container>
+    </BgImage>
   );
 }
---
+++ src/pages/SignUp/styles.js
@@ -0,0 +1,92 @@
+import styled from 'styled-components';
+import theme from '../../styles/theme';
+import bgLight from '../../assets/images/bg-pattern-light.svg';
+
+export const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+
+  p {
+    align-self: center;
+    margin-top: 18px;
+    color: ${theme.text};
+
+    strong {
+      margin-left: 10px;
+      font-weight: 700;
+
+      a {
+        color: ${theme.text};
+      }
+    }
+  }
+`;
+
+export const Wrapper = styled.div`
+  margin-top: 100px;
+  border-radius: 4px;
+  background: white;
+  min-width: 470px;
+  height: 520px;
+`;
+
+export const SignUpHeader = styled.div`
+  background: ${theme.primary};
+  height: 94px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  padding: 0 10px;
+
+  img {
+    margin-top: -15px;
+  }
+`;
+
+export const BgImage = styled.div`
+  background: url(${bgLight}) no-repeat center center fixed;
+  background-size: cover;
+  height: 100%;
+  overflow: hidden;
+`;
+
+export const SignUpContent = styled.div`
+  background: #fff;
+  display: flex;
+  margin: 28px 37px;
+  flex-direction: column;
+
+  h2 {
+    align-self: center;
+    margin-bottom: 8px;
+    color: ${theme.text};
+  }
+  p {
+    align-self: center;
+    margin-bottom: 35px;
+    color: ${theme.text};
+  }
+  input {
+    margin-bottom: 15px;
+    height: 37px;
+    padding-left: 12px;
+    border-radius: 3px;
+    outline: 0;
+    border: solid 1px rgba(0, 0, 0, 0.1);
+    color: ${theme.text};
+  }
+  button {
+    margin-bottom: 4px;
+    background: ${theme.primary};
+    height: 35px;
+    width: 100px;
+    color: #fff;
+    align-self: center;
+    font-weight: 700;
+  }
+`;
